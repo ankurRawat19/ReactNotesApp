9372361881ab6bdcd6ae72fc46be3a272ac8420e
Ankur Rawat <ankur.rawat@cavisson.com>
2023-08-08 12:07:14 +0530

    Updates

---
+++ precache-manifest.822334305d53c855a6b7c7b749dfeb4e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d58f7d0d0c734f67ab77",
+    "url": "/ReactNotesApp/static/css/main.d370a759.chunk.css"
+  },
+  {
+    "revision": "d58f7d0d0c734f67ab77",
+    "url": "/ReactNotesApp/static/js/main.d58f7d0d.chunk.js"
+  },
+  {
+    "revision": "a2abdf72fbe066d3b1bd",
+    "url": "/ReactNotesApp/static/js/1.a2abdf72.chunk.js"
+  },
+  {
+    "revision": "a6c702380aa5494e59ba",
+    "url": "/ReactNotesApp/static/js/runtime~main.a6c70238.js"
+  },
+  {
+    "revision": "d8395f9df4f25c3ec7522c0f6d502e72",
+    "url": "/ReactNotesApp/index.html"
+  }
+];
